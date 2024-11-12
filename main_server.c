e7be04e55cdb271b1f8449d2e9106c893d411999
jaeyu5034 <pyyp5034@gmail.com>
2024-11-12 14:15:41 +0900

    send temp data to client

--- main_server.c
+++ main_server.c
@@ -4,9 +4,15 @@
 #include <unistd.h>
 #include <wiringPi.h>
 #include <sys/socket.h>
+#include <arpa/inet.h>
+#include <string.h>
 
 #define DHTPIN 7  
 #define MAX 85
+#define PORT 9090
+#define BUF_SIZE 1024
+
+void error_handling(char *message);
 
 int dht_val[5] = {0, 0, 0, 0, 0};
 
@@ -55,7 +61,7 @@ int read_dht22_data() {
     }
 
     
-    if ((j >= 40) && (dht_val[4] == ((dht_val[0] + dht_val[1] + dht_val[2] + dht_val[3]) & 0xFF))) {
+    if ((j >= 40) && (dht_val[4] == ((dht_val[0] + dht_val[1] + dht_val[2] + dht_val[3]) & 0xFF))) {//데이터 에러 없다면 1, 있으면 0
         return 1;  
     } else {
         return 0;  
@@ -63,32 +69,91 @@ int read_dht22_data() {
 }
 
 
-void print_dht22_data() {
-    float humidity = ((dht_val[0] << 8) + dht_val[1]) / 10.0;
-    float celsius = ((dht_val[2] << 8) + dht_val[3]) / 10.0;
-    float fahrenheit = celsius * 9.0 / 5.0 + 32;
 
-    printf("현재 연구실 습도 = %.1f %% 현재 연구실 온도 = %.1f *C (%.1f *F)\n", humidity, celsius, fahrenheit);
-}
+void handle_client(int cli_sock){
+    char response[BUF_SIZE];
+    char html_content[BUF_SIZE];
 
-int main(void) {
+    if(read_dht22_data()){
+        float humidity = ((dht_val[0]<<8) + dht_val[1]) / 10.0;
+        float celsius = ((dht_val[2]<<8) + dht_val[3]) /10.0;
 
-    if (wiringPiSetup() == -1) {
-        printf("WiringPi setup error!\n");
-        return -1;
+        snprintf(html_content, sizeof(html_content), 
+            "<html><body>"
+            "<h1>N4동 413호 온도와 습도</h1>"
+            "<p>온도: %.1f &deg;C</p>"
+            "<p>습도: %.1f%%</p>"
+            "</body></html>", celsius, humidity);
+        
+        snprintf(response, sizeof(response),
+            "HTTP/1.1 200 OK\r\n"
+            "Content-Type: text/html; charset=UTF-8\r\n"
+            "Content-Length: %zu\r\n"
+            "Connection: close\r\n"
+            "\r\n"
+            "%s",
+            strlen(html_content), html_content);
+    }
+    else{   //실패시 -> 읽을 데이터 없을때
+        snprintf(response, sizeof(response),
+            "HTTP/1.1 500 Internal Server Error\r\n"
+            "Content-Type: text/html; charset=UTF-8\r\n"
+            "Connection: close\r\n"
+            "\r\n"
+            "<html><body><h1>데이터 오류 발생</h1></body></html>");
     }
 
-    printf("Starting DHT22 sensor data read...\n");
+    write(cli_sock, response, strlen(response));
+    close(cli_sock);
+}
+
+int main(void){
+
+    int serv_sock, cli_sock;
+    struct sockaddr_in serv_addr, cli_addr;
+    socklen_t cli_addr_len;
+
+    if (wiringPiSetup() == -1) {
+        error_handling("wiringPiSetup() error");
+    }
+
+    serv_sock = socket(AF_INET, SOCK_STREAM, 0);
+    if(serv_sock==-1){
+        error_handling("socket() error");
+    }
+    memset(&serv_addr, 0x00, sizeof(serv_addr));
+    serv_addr.sin_family = AF_INET;
+    serv_addr.sin_addr.s_addr = htonl(INADDR_ANY);
+    serv_addr.sin_port = htons(PORT);
+
+    if(bind(serv_sock, (struct sockaddr*)&serv_addr, sizeof(serv_addr))==-1){
+        error_handling("bind() erorr");
+    }
+
+    if(listen(serv_sock, 5)==-1){
+        error_handling("listen() error");
+    }
+
+    printf("Server running on port %d....\n", PORT);
 
 
     while (1) {
-        if (read_dht22_data()) {
-            print_dht22_data();
-        } else {
-            printf("Failed to read DHT22 data\n");
+        cli_addr_len = sizeof(cli_addr);
+        cli_sock = accept(serv_sock, (struct sockaddr*)&cli_addr, &cli_addr_len);
+        if(cli_sock==-1){
+            perror("accpet() error");
+            continue;
         }
-        sleep(5);  
+        handle_client(cli_sock);
+
     }
 
+    close(serv_sock);
     return 0;
+}
+
+void error_handling(char *message){
+    fputs(message, stderr);
+    fputc('\n', stderr);
+    exit(1);
 }
